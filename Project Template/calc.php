5268e737796b6abd1847ec91ff397c4a87b9c4c6
cancel246 <35869407+cancel246@users.noreply.github.com>
2019-04-16 16:36:13 -0700

    updated application
    almost ready for testing, just needs scraper (Mach still working on it)

--- Project Template/calc.php
+++ Project Template/calc.php
@@ -1,5 +1,9 @@
 <!DOCTYPE HTML>
 
+<?php
+    
+?>
+
 <html>
 	<head>
 		<title>RenewBim - Energy Efficiency Calculator</title>
@@ -89,14 +93,15 @@
                         
 						<h2 id="energy">Energy Efficiency Calculator</h2>
 					</header>
+                    <span id="a">Your <br/> Appliance</span>
+                    <span id="b">Your Appliance with Renewable Energy</span>
+                    <span id="c">Energy Efficient Appliance with Renewable Energy</span>
                     <br/><br/><br/>
+                    
+                    <!-- Table 1 -->
+                    
                     <table>
-                        <tr>
-                            <th></th>
-                            <th>Your Appliance</th>
-                            <th>Your Appliance with renewable energy</th>
-                            <th>Energy Efficent Appliance with renewable energy</th>
-                        </tr>
+                        <tr><th></th><th></th></tr>
                         <tr>
                             <td>Model Number</td>
                             <td>
@@ -111,27 +116,25 @@
                                 ?>
                             
                             </td>
-                            <td></td>
-                            <td></td>
                         </tr>
                         
                         <tr>
                             <td>Type</td>
                             <td>
                                 <?php
+                                $varType = "";
+                                
                                     $type = "SELECT ItemType FROM appliances WHERE ModelNum = '$model'";
                                 
                                 $result1 = $conn->query($type);
                                 
                                 while($row = mysqli_fetch_array($result1)){
                                     echo $row[0];
+                                    $varType = $row[0];
                                 }
                                 ?>
                             </td>
-                            <td></td>
-                            <td></td>
                         </tr>
-                        
                         <tr>
                             <td>Brand Name</td>
                             <td>
@@ -145,45 +148,279 @@
                                 }
                                 ?>
                             </td>
-                            <td></td>
-                            <td></td>
                         </tr>
                         <tr>
                         <td>Wattage (kWh)</td>
                             <td>
                                 <?php
+                                    $varWatts = 0.00;
                                     $watts = "SELECT Watts FROM appliances WHERE ModelNum = '$model'";
                                 
                                 $result3 = $conn->query($watts);
                                 
                                 while($row = mysqli_fetch_array($result3)){
                                     echo $row[0];
+                                    $varWatts = $row[0];
                                 }
                                 ?>
                             </td>
-                            <td></td>
-                            <td></td>
                         </tr>
                         <tr>
-                            <td>Cost per day(24hrs)</td>
+                            <td>Monthly Cost</td>
                             <td>
                                 <?php
-                                    $day = 24.00;
+                                    $varUse = 0.00;
+                                    $varCost = 0.00;
+                                    $use = "SELECT DailyUse FROM appliances where ModelNum = '$model'";
+                                
+                                    $result4 = $conn->query($use);
+                                
+                                    while($row = mysqli_fetch_array($result4)){
+                                    $varUse = $row[0]; 
+                                    $varCost = $varWatts * $varUse * 0.176 * 30;   
+                                    echo "$".number_format((float)$varCost, 2, '.', '');
+                                }
+                                    
+                                 
+                                ?>
+                            </td>    
+                        </tr>
+                        <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+                        <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+                        <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+                    </table>
+                    
+                    
+                    <!-- Table 2 -->
+                
+                    <table>
+                        <tr><th></th><th></th></tr>
+                        <tr>
+                            <td>Installation & Purchase Cost</td>
+                            <td>                     <!-- Query need to change-->
+                                <?php
+                                    $cost = "SELECT cost FROM solarpanel WHERE id = 'solarpanel2' ";
+                                
+                                $result = $conn->query($cost);
+                                
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "$".number_format((float)$row[0], 2, '.', '');
+                                    
+                                }
+                                ?>
+                            
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                            <td>Number of Solar Panels</td>
+                            <td> <!-- set num to be an if statement based on above-->
+                               <?php 
+                                    /*$num = "SELECT Brand FROM appliances WHERE ModelNum = '$model'";
+                                
+                                $result2 = $conn->query($brand);
+                                
+                                while($row = mysqli_fetch_array($result2)){
+                                    echo $row[0];
+                                } */
+                                
+                                echo 2;
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Brand Name</td>
+                            <td>
+                               <?php
+                                    $brand = "SELECT Brand FROM appliances WHERE ModelNum = '$model'";
+                                
+                                $result2 = $conn->query($brand);
+                                
+                                while($row = mysqli_fetch_array($result2)){
+                                    echo $row[0];
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                            <td>Wattage Generated</td>
+                            <td>
+                               <?php
+                                    $varValue = 0.00;
+                                    $sWatts = "SELECT kWh FROM solarpanel WHERE id = 'solarpanel1'";
+                                
+                                $result2 = $conn->query($sWatts);
+                                while($row = mysqli_fetch_array($result2)){
+                                    echo $row[0];
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                        <td>Appliance Wattage with Renewable(kWh)</td> 
+                            <td> <!-- Query need to change to suit-->
+                                <?php
+                                    $varSWatts = 0.00;
+                                    $watts = "SELECT kWh FROM solarpanel WHERE id = 'solarpanel1'";
+                                
+                                $result3 = $conn->query($watts);
+                                
+                                while($row = mysqli_fetch_array($result3)){
+                                    $varSWatts = $row[0];
+                                    $varSWatts = $varWatts - $varSWatts;
+                                    echo $varSWatts;
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Monthly Savings</td>
+                            <td>
+                                <?php
+                                    $varSavings = 0.00;
+                                    $use = "SELECT DailyUse FROM appliances where ModelNum = '$model'";
+                                    
+                                    $result3 = $conn->query($use);
                                     while($row = mysqli_fetch_array($result3)){
-                                    $num = $row[0];
+                                    $varUse = $row[0];
                                         
-                                        echo $num * $day;
+                                    if($varSWatts < 0){
+                                        $varSavings = $varCost;
+                                        echo "$".number_format((float)$varSavings, 2, '.', '');
+                                    }else{
+                                        $varSavings = $varSWatts * $varUse * 0.176 * 30;
+                                        
+                                        $varSavings = $varCost - $varSavings;
+                                        echo "$".number_format((float)$varSavings, 2, '.', '');
+                                    }
                                 }
                                 
                                  
                                 ?>
-                            </td>
-                            <td></td>
-                            <td></td>    
+                            </td>    
                         </tr>
-                            
-                        
+                        <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
                     </table>
+                    
+                    
+                    <!-- Table 3 -->
+                   
+                    <table>
+                        <tr><th></th><th></th></tr>
+                        <tr>
+                            <td>Type</td>
+                            <td>
+                                <?php
+                                    $typeE = "SELECT Type FROM efficient WHERE Type = '$varType'";
+                                
+                                $result1 = $conn->query($typeE);
+                                
+                                while($row = mysqli_fetch_array($result1)){
+                                    echo $row[0];
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Brand Name</td>
+                            <td>
+                               <?php
+                                    $brand = "SELECT Brand FROM efficient WHERE Type = '$varType'";
+                                
+                                $result2 = $conn->query($brand);
+                                
+                                while($row = mysqli_fetch_array($result2)){
+                                    echo $row[0];
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                            <td>Total Cost</td>
+                            <td>
+                                <?php
+                                $varCostS = 0.00;
+                                $varCostE = 0.00;
+                                $varCost = 0.00;
+                                
+                                    $costS = "SELECT cost FROM solarpanel WHERE id = 'solarpanel1'";
+                                
+                                $result = $conn->query($costS);
+                                
+                                while($row = mysqli_fetch_array($result)){
+                                    $varCostS = $row[0];
+                                }
+                                $costE = "SELECT Cost FROM efficient WHERE Type = '$varType'";
+                                
+                                $result = $conn->query($costE);
+                                
+                                while($row = mysqli_fetch_array($result)){
+                                    $varCostE = $row[0];
+                                    $varCost = $varCostE + $varCostS;
+                                    echo "$".number_format((float)$varCost, 2, '.', '');
+                                }
+                                
+                                ?>
+                            
+                            </td>
+                        </tr> 
+                        <tr>
+                            <td>Cost Difference</td>
+                            <td>
+                                <?php
+                                $varDiff = 0.00;    
+                                    $costDev = "SELECT Cost FROM appliances WHERE ModelNum = '$model'";
+                                
+                                $result = $conn->query($costDev);
+                                
+                                while($row = mysqli_fetch_array($result)){
+                                    $varDiff = $varCost - $row[0];
+                                    
+                                    echo "$".number_format((float)$varDiff, 2, '.', '');
+                                }
+                                ?>
+                            
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                        <td>Wattage of Device(kWh)</td> 
+                            <td>
+                                <?php
+                                    $wattS = 0.00;
+                                    $watts = "SELECT Watts FROM efficient WHERE Type = '$varType'";
+                                
+                                $result3 = $conn->query($watts);
+                                
+                                while($row = mysqli_fetch_array($result3)){
+                                    echo $row[0];
+                                    $wattS = $row[0];
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Monthly Savings</td> <!-- To change to monthly-->
+                            <td>
+                                <?php
+                                    $use = "SELECT DailyUse FROM appliances where ModelNum = '$model'";
+                                    $result3 = $conn->query($use);
+                                    while($row = mysqli_fetch_array($result3)){
+                                        $amtSaved = $wattS * $row[0] * 0.176 * 30;
+                                        echo "$".number_format((float)$amtSaved, 2, '.', '');
+                                }
+                                
+                                 
+                                ?>
+                            </td>    
+                        </tr>
+                        <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+                        <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+                    </table>
+                   
                     <br/><br/><br/><br/><br/><br/>
 					<form action="calc.php" method="post">
                     Enter another appliance model number if you wish to search more:     <br/><br/>
@@ -215,38 +452,38 @@
 						</div>
 						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
-							<h3>Khalid Clarke</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
+							<h3>Terrell Davis</h3>
+							<p>Designer</p>
 						</div>
 						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
 							<h3>Delano Frederick</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
+							<p>Designer</p>
+						</div>
+						<div class="3u">
+							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
+							<h3>Khalid Clarke</h3>
+							<p>Programmer</p>
+						</div> <br/>
+                        <div class="3u">
+							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
+							<h3>Cherese Sealy</h3>
+							<p>Programmer</p>
 						</div>
 						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
 							<h3>Akeem Greenidge</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
-						</div> <br/>
-                        <div class="3u">
+							<p>Business Analyst</p>
+						</div>
+						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
 							<h3>Che Roach</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
+							<p>Business Analyst</p>
 						</div>
 						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
-							<h3>Henry Dorsett Case</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
-						</div>
-						<div class="3u">
-							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
-							<h3>Willis Corto</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
-						</div>
-						<div class="3u">
-							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
-							<h3>Linda Lee</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
+							<h3>Kevin Yarde</h3>
+							<p>QA Tester</p>
 						</div>
 					</div>
 				</section>
